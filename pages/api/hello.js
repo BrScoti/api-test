3f281f5c3f92cce5667943ce7b4aeda73a1d9f7c
Bruno Scoti <bruno.scoti@gmail.com>
2022-10-09 22:38:44 -0300

    change api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ req, res })
+  //console.log(req);
+  res.status(200).json({ headers: req.headers, body: req.body })
 }

3e3f161bf2609d7f8d8d8f6288f31a5f8678fecf
Bruno Scoti <bruno.scoti@gmail.com>
2022-10-09 22:42:41 -0300

    print req, res

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  //console.log(req);
+  console.log({ headers: req.headers, body: req.body });
   res.status(200).json({ headers: req.headers, body: req.body })
 }

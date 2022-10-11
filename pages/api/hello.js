8b331e87b572be73496f3d5b6bef4d484912a9f0
Bruno Scoti <bruno.scoti@gmail.com>
2022-10-10 21:24:28 -0300

    adding refusal reason

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  console.log({ headers: req.headers, body: req.body });
+  console.log({ headers: req.headers, refusal_reason: req.body.purchase.payment.refusal_reason });
   res.status(200).json({ headers: req.headers, body: req.body })
 }

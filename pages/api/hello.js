a0c4ef231628eb19ced9de5c1e187b6c1c4fcd4f
Bruno Scoti <bruno.scoti@gmail.com>
2022-10-10 21:26:55 -0300

    json stringify

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  console.log({ headers: req.headers, refusal_reason: req.body.purchase.payment.refusal_reason });
+  console.log(JSON.stringify(req.body))
+  //console.log({ headers: req.headers, refusal_reason: req.body.purchase.payment.refusal_reason });
   res.status(200).json({ headers: req.headers, body: req.body })
 }
